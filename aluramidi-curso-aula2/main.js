148a6da0e8e7ee050d7716e1834a49eff1652543
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2023-10-16 10:10:29 -0300

    att

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,10 +1,10 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function idElemento Audio () {
+    document.querySelector(idElemento Audio).play();
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla);
 let contador = 0
 while (contador < 9) {
-    listadeTeclas[contador].onclick = TocaSomPom;
+    listadeTeclas[contador].onclick = TocaSom;
     contador = contador + 1;
 }
