f3fed39f181eb12788b67e009524c43b94aab19a
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2023-11-06 10:07:13 -0300

    .

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -2,7 +2,7 @@ function idElemento Audio () {
     document.querySelector(idElemento Audio).play();
 }
 
-const listadeTeclas = document.querySelectorAll('.tecla);
+const listadeTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla= listaDeTeclas[contador];
@@ -10,6 +10,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const id Audio =`#som_$(instrumento)`;
     tecla.onclick = function(){
     TocaSom(id Audio)
-}
+    }
+
     contador = contador + 1;
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'enter')
+        tecla.classList.add('ativa')
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
 }
+}
+
+
